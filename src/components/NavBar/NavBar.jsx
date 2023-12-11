de6b13a51fc1b24eca59112f1282583d7212ee2b
AndriiMatsiuk <amacyuk2@gmail.com>
2023-12-11 16:48:56 +0200

    first

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+export const NavBar = () => {
+  return (
+    <nav className='nav'>
+      <div>Profile</div>
+      <div>Massages</div>
+      <div>News</div>
+      <div>Music</div>
+      <div>Settings</div>
+    </nav>
+  );
+};
